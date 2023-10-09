a6de7a82fb196b1ee1fc297c96cc5b43d04ec3b4
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-09 20:56:07 -0300

    vendo sobre o enum no typeScript

--- code/index.ts
+++ code/index.ts
@@ -1,6 +1,12 @@
 
-// Apple, 3, true
 
-let item: [string, number, boolean] = ['Aplle', 3, true]
+//const admin = 1
+//const rad = 2
+//const backup = 3
 
-item[1].toString
+enum Role{admin = 1, read = 2, backup = 3}
+const user = {
+   firsName: 'Marcos',
+   age: 40,
+   role: Role.backup
+}
