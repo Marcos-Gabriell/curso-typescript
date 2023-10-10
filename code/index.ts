38685c53d3c4f805d20b280a3dc3916d35dd80d1
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-09 21:02:29 -0300

    vendo sobre function return

--- code/index.ts
+++ code/index.ts
@@ -1,12 +1,4 @@
 
-
-//const admin = 1
-//const rad = 2
-//const backup = 3
-
-enum Role{admin = 1, read = 2, backup = 3}
-const user = {
-   firsName: 'Marcos',
-   age: 40,
-   role: Role.backup
+function add(num1: number, num2: number):boolean   {
+   return num1 + num2
 }
