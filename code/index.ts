7eab6df8ca2cb6feb5ef2547d6609e84224b6a39
Marcos Gabriel <marcosgabriel79355@gmail.com>
2023-10-09 21:13:40 -0300

    vendo sobre function void

--- code/index.ts
+++ code/index.ts
@@ -1,4 +1,10 @@
 
-function add(num1: number, num2: number):boolean   {
+function add(num1: number, num2: number)   {
    return num1 + num2
 }
+
+function result(price: number) {
+   console.log('The resulti is' + price)
+}
+
+console.log(result(add(6,5)))
