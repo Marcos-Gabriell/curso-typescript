1e130f1d94b54766956b3a63deb36e0d57deaad9
Marcos Gabriel <marcosgabriel79355@gmail.com>
2024-01-31 09:39:28 -0300

    finalziando o dia 6

--- dia-6/code/index.ts
+++ dia-6/code/index.ts
@@ -1,22 +1,12 @@
-// Day 5 - Exercise 5
 
-interface UserSchema {
-    id: number;
-    name: string;
+class Moveis {
+    id: number
+    name: string
+
+    constructor(id:number, name: string) {
+        this.id = id
+        this.name = name
+    }
 }
 
-class User implements UserSchema {
-    constructor(public name: string, public id: number) {}
-}
-
-const user = new User('Dog', 1);
-
-console.log(user.id);
-
-// Atribuição direta de 'id' não é permitida pois 'id' é uma propriedade somente de leitura
-// user.id = 5; // Erro: 'id' é uma propriedade somente de leitura
-
-// Alterando o nome do usuário é permitido
-user.name = 'Harold';
-
-console.log(`User:`, user);
+let movei1 = new Moveis(1, 'Dea')
